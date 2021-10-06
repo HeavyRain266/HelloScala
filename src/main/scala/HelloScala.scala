0f6baa57783a50757917b3f1a9b7035f80cb7970
HeavyRain266 <ignacy423@gmail.com>
2021-10-06 23:30:26 +0200

    fix build by removing unecessary KeyHandler class.
    I don't really need to handler keybindings for something simple like that, and will never need it.

--- src/main/scala/HelloScala.scala
+++ src/main/scala/HelloScala.scala
@@ -7,11 +7,11 @@ import org.lwjgl.opengl.GL11._
 import org.lwjgl.system.MemoryUtil._
 
 class HelloScala {
-  private object Win {
-    var instance: Long = 0
+  private object Window {
     val title: String  = "Hello, Scala!"
     val width: Int     = 800
     val height: Int    = 600
+    var instance: Long = 0
   }
 
   def start(): Unit = {
@@ -19,7 +19,7 @@ class HelloScala {
       init()
       loop()
 
-      glfwDestroyWindow(Win.instance)
+      glfwDestroyWindow(Window.instance)
     } finally {
       glfwTerminate()
     }
@@ -33,15 +33,14 @@ class HelloScala {
     }
 
     glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err))
-    glfwSetKeyCallback(Win.instance, new KeyboardHandler())
 
     glfwDefaultWindowHints()
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
 
-    Win.instance =
-      glfwCreateWindow(Win.width, Win.height, Win.title, NULL, NULL)
-    if (Win.instance == NULL) {
+    Window.instance =
+      glfwCreateWindow(Window.width, Window.height, Window.title, NULL, NULL)
+    if (Window.instance == NULL) {
       throw new RuntimeException(
         "Failed to create new GLFW window instance!"
       )
@@ -51,14 +50,14 @@ class HelloScala {
 
     if (videoMode != null) {
       glfwSetWindowPos(
-        Win.instance,
-        (videoMode.width() - Win.width) / 2,
-        (videoMode.height() - Win.height) / 2
+        Window.instance,
+        (videoMode.width() - Window.width) / 2,
+        (videoMode.height() - Window.height) / 2
       )
     }
-    glfwMakeContextCurrent(Win.instance)
+    glfwMakeContextCurrent(Window.instance)
     glfwSwapInterval(1)
-    glfwShowWindow(Win.instance)
+    glfwShowWindow(Window.instance)
   }
 
   private def loop(): Unit = {
@@ -66,9 +65,9 @@ class HelloScala {
 
     glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
 
-    while (!glfwWindowShouldClose(Win.instance)) {
+    while (!glfwWindowShouldClose(Window.instance)) {
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-      glfwSwapBuffers(Win.instance)
+      glfwSwapBuffers(Window.instance)
       glfwPollEvents()
     }
   }
